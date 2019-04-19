3c152d073fa4bf0e1c6315b00763cda805c75f6a
vikzh <vikt.anmail@gmail.com>
2019-04-19 11:52:23 +0300

    tests improvement

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,27 +2,14 @@ import fs from 'fs';
 import gendiff from '../src';
 
 describe('gendiff', () => {
-  test('Json', () => {
-    const filePathBefore = './__tests__/__fixtures__/json/before.json';
-    const filePathAfter = './__tests__/__fixtures__/json/after.json';
-    const fileExpected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
+  const fileExpected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
+  const flatJson = ['./__tests__/__fixtures__/json/before.json', './__tests__/__fixtures__/json/after.json', fileExpected];
+  const flatYaml = ['./__tests__/__fixtures__/yml/before.yml', './__tests__/__fixtures__/yml/after.yml', fileExpected];
+  const flatIni = ['./__tests__/__fixtures__/ini/before.ini', './__tests__/__fixtures__/ini/after.ini', fileExpected];
 
-    expect(gendiff(filePathBefore, filePathAfter)).toEqual(fileExpected);
-  });
-
-  test('YAML', () => {
-    const filePathBefore = './__tests__/__fixtures__/yml/before.yml';
-    const filePathAfter = './__tests__/__fixtures__/yml/after.yml';
-    const fileExpected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
-
-    expect(gendiff(filePathBefore, filePathAfter)).toEqual(fileExpected);
-  });
-
-  test('Ini', () => {
-    const filePathBefore = './__tests__/__fixtures__/ini/before.ini';
-    const filePathAfter = './__tests__/__fixtures__/ini/after.ini';
-    const fileExpected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
-
-    expect(gendiff(filePathBefore, filePathAfter)).toEqual(fileExpected);
-  });
+  test.each([flatJson, flatYaml, flatIni])(
+    '.files(%s, %s)', (firstFile, secondFile, expectedFile) => {
+      expect(gendiff(firstFile, secondFile)).toEqual(expectedFile);
+    },
+  );
 });
