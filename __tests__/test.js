1f3428561312c939a49e741cd2e38f4478c57099
vikzh <vikt.anmail@gmail.com>
2019-04-26 15:07:25 +0300

    tests added

--- __tests__/test.js
+++ __tests__/test.js
@@ -20,6 +20,25 @@ describe('gendiff', () => {
     [pathToYamlBefore, pathToYamlAfter],
   ];
 
+  const simpleNestedExpected = fs.readFileSync('./__tests__/__fixtures__/simple-nested-expected', 'utf-8');
+  const plainNestedExpected = fs.readFileSync('./__tests__/__fixtures__/plain-nested-expected', 'utf-8');
+  const jsonNestedExpected = fs.readFileSync('./__tests__/__fixtures__/json-nested-expected', 'utf-8');
+
+  const pathToNestedJsonBefore = './__tests__/__fixtures__/json/nested-before.json';
+  const pathToNestedJsonAfter = './__tests__/__fixtures__/json/nested-after.json';
+
+  const pathToNestedYamlBefore = './__tests__/__fixtures__/yml/nested-before.yml';
+  const pathToNestedYamlAfter = './__tests__/__fixtures__/yml/nested-after.yml';
+
+  const pathToNestedIniBefore = './__tests__/__fixtures__/ini/nested-before.ini';
+  const pathToNestedIniAfter = './__tests__/__fixtures__/ini/nested-after.ini';
+
+  const nestedFiles = [
+    [pathToNestedJsonBefore, pathToNestedJsonAfter],
+    [pathToNestedIniBefore, pathToNestedIniAfter],
+    [pathToNestedYamlBefore, pathToNestedYamlAfter],
+  ];
+
   test.each(simpleFiles)(
     'simple format, simple files .files(%s, %s)', (firstFile, secondFile) => {
       expect(gendiff(firstFile, secondFile, 'tree')).toEqual(simpleExpected);
@@ -29,6 +48,24 @@ describe('gendiff', () => {
   test.each(simpleFiles)(
     'plain format, simple files .files(%s, %s)', (firstFile, secondFile) => {
       expect(gendiff(firstFile, secondFile, 'plain')).toEqual(plainExpected);
+    },
+  );
+
+  test.each(nestedFiles)(
+    'simple format, nested files .files(%s, %s)', (firstFile, secondFile) => {
+      expect(gendiff(firstFile, secondFile, 'tree')).toEqual(simpleNestedExpected);
+    },
+  );
+
+  test.each(nestedFiles)(
+    'plain format, simple files .files(%s, %s)', (firstFile, secondFile) => {
+      expect(gendiff(firstFile, secondFile, 'plain')).toEqual(plainNestedExpected);
+    },
+  );
+
+  test.each(nestedFiles)(
+    'json format, simple files .files(%s, %s)', (firstFile, secondFile) => {
+      expect(gendiff(firstFile, secondFile, 'json')).toEqual(jsonNestedExpected);
     },
   );
 });
